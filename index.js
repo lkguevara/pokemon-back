321e29c2d4f2443736eba36130ff0d775cd6c6e7
Lian Katherin Guevara <liann.guevara@gmail.com>
2023-03-06 17:21:23 -0500

    fix bug deploy

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const getAllTypes = require("./src/controllers/types"); // con esto nos evitamos hacer un get de types cada vez que entregmos al navegador para que la tabla de relaciones se actualice y cree las relaciones
+const port = process.env.PORT || 3001;
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('Port listening at 3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT, async () => {
+    console.log(`server listening in port ${port}`); // eslint-disable-line no-console
   });
 });
